9b4f4720639adcfb537284f5e718ec389261f514
Albert Català <albert.catala@rakuten.com>
2021-09-01 23:55:31 +0200

    Create spec for discount on item price by units calculation

---
+++ spec/lib/shopping_cart/pricing_calculation/discount_on_item_price_by_units_calculation_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+RSpec.describe ShoppingCart::
+                PricingCalculation::
+                DiscountOnItemPriceByUnitsCalculation do
+  subject { described_class.new(product.price, arguments).total(quantity) }
+
+  let(:arguments) { { percent_discount: 33.33, units: 3 } }
+  let(:product) { Product.new(code: 'CF1', name: 'Coffee', price: 11.23) }
+
+  describe '#total' do
+    context 'when the quantity is less than the units of the rule' do
+      let(:quantity) { 2 }
+
+      it 'returns the price of the product * quantity' do
+        expect(subject).to eq(22.46)
+      end
+    end
+
+    context 'when the quantity is equal than the units of the rule' do
+      let(:quantity) { 3 }
+
+      it 'returns the price of the product decremented by the discount '\
+         '* quantity' do
+        expect(subject).to eq(22.461123)
+      end
+    end
+
+    context 'when the quantity is grather than the units of the rule' do
+      let(:quantity) { 4 }
+
+      it 'returns the price of the product decremented by the discount '\
+         '* quantity' do
+        expect(subject).to eq(29.948164)
+      end
+    end
+  end
+end
